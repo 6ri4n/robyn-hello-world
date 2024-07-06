5e1e73fc096299fd217dbd21fc7e768464f3e913
brian <bri4n.huang@gmail.com>
2024-07-06 12:59:07 -0700

    update

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from robyn import Robyn
+from dotenv import load_dotenv
+import os
 
 app = Robyn(__file__)
-
+load_dotenv()
+port = os.getenv("PORT") or 8080
 
 @app.get("/")
 def index():
@@ -9,4 +12,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.start(host="0.0.0.0", port=8080)
+    app.start(host="0.0.0.0", port=port)
